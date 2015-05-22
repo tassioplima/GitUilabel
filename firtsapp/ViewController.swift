8bdd4b5074b262be564cfb57684e7cde35a6a006
tassioplima <tassiopelima@gmail.com>
2015-05-22 11:01:48 -0300

    projeto

---
+++ firtsapp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  firtsapp
+//
+//  Created by Tassio Lima on 18/05/15.
+//  Copyright (c) 2015 Tassio Lima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
